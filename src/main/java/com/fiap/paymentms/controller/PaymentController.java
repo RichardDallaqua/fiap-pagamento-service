6bcf67557391b088b0e9de4a4fdc6897d7afadb5
Andre Furtado <furtado11135@gmail.com>
2024-03-19 23:55:41 -0300

    ajustes

--- src/main/java/com/fiap/paymentms/controller/PaymentController.java
+++ src/main/java/com/fiap/paymentms/controller/PaymentController.java
@@ -7,13 +7,7 @@ import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/payments/")
@@ -23,7 +17,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @GetMapping("/{order-id}")
-    public OrderVO getByOrderIdentifier(@PathParam("order-id") String orderIdentifier){
+    public OrderVO getByOrderIdentifier(@PathVariable("order-id") String orderIdentifier){
         return paymentService.findByOrderIdentifierAndBuildVO(orderIdentifier);
     }
 }
